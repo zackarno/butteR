8d4568f2850c677c7ca42545aa3d1bf60f6e2674
zackarno <zackarno@gmail.com>
2019-10-14 00:17:41 +0600

    strsplit_on_multiple function

---
+++ R/strsplit_on_multiple.R
@@ -0,0 +1,24 @@
+#'
+#'
+#'Split strings on multiple delimeters
+#'
+#' @param x string or vector of strings
+#' @param separator single or vector of separator delimeters
+#' @return a list of vectors with split strings
+#' @details does not support regex matching
+#' @examples
+#' strings_to_split<-c('abc def.gh', "def.kl abc 9")
+#' separation_pattern<-c(" ", "c")
+#' strsplit_on_multiple(x = strings_to_split,
+#'                      separation_pattern)
+#'
+#'
+strsplit_on_multiple<-function(x,separator){
+
+  separator<- Hmisc::escapeRegex(separator)
+
+  collapsed_separator<-paste0( separator, collapse= "|")
+  split_element<- strsplit(x=x,split = collapsed_separator) %>%
+    lapply(function(x) x[x!=""]) #remove any empty returned strings
+  split_element
+}
