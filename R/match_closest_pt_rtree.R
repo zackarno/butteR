cb7c2c112badaabe2554375531080b024a5518bf
zackarno <zackarno@gmail.com>
2020-01-29 09:15:43 +0300

    added package imports to description

--- R/match_closest_pt_rtree.R
+++ R/match_closest_pt_rtree.R
@@ -5,6 +5,8 @@
 #' @param return_distance return the closest point with a vector indicating distance (m). Default = TRUE
 #' @return sf1 with closest record from sf2 attached.
 #' @export
+#' @importFrom rtree Rtree knn knn.Rtree
+#' @importFrom sf st_distance
 
 match_closest_pt_rtree <- function(sf1, sf2, return_distance=TRUE, k=1) {
   sf2_tree<- rtree::RTree(st_coordinates(sf2))
@@ -23,9 +25,11 @@ match_closest_pt_rtree <- function(sf1, sf2, return_distance=TRUE, k=1) {
 #' @param sf2 simple feature 2 (point).
 #' @return sf1 with closest record from sf2 attached.
 #' @export
+#' @importFrom rtree Rtree knn knn.Rtree
+#' @importFrom sf st_distance
 
 
-closest_distance_rtree<-function(sf1, sf2, k=1) {
+closest_distance_rtree<-function(sf1, sf2, k=1,sf1_coords=c("X","Y")) {
   sf2_tree<- rtree::RTree(st_coordinates(sf2)[,c("X", "Y")])
   knn1_index<-rtree::knn.RTree(rTree=sf2_tree,st_coordinates(sf1)[,c("X", "Y")], k=1)
   closest_distance<- sf::st_distance(sf1,sf2[unlist(knn1_index),], by_element = TRUE)
