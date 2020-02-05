4b4045500486f7b0312e1e21a195f94c82effe4e
zackarno <zackarno@gmail.com>
2020-02-05 13:08:53 +0300

    add st_drop_geometry_keep_coords

---
+++ R/st_drop_geometry_keep_coords.R
@@ -0,0 +1,12 @@
+#' Wraps SF st_drop_geometry but retains coordinates as first column in data.frame
+#' @param sf_objec simple_feature object
+#' @return sf object as data frame with coordinates as first column
+#' @export
+
+
+st_drop_geometry_keep_coords<-function(sf_object){
+  coords<-sf::st_coordinates(sf_object)
+  df_object<-sf_object %>% st_drop_geometry()
+  df_object[,colnames(coords)]<-coords
+  df_object<- df_object %>% dplyr::select(colnames(coords), everything())
+}
