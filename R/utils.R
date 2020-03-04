d711cc45f0490927d609b79ae101dc451a21e0c1
zackarno <zackarno@gmail.com>
2020-03-04 17:40:27 +0300

    added max_prefix_length argument to  remove_kobo_grouper

--- R/utils.R
+++ R/utils.R
@@ -6,6 +6,19 @@
 
 
 
-remove_kobo_grouper<-function(colname){
-  sub(".*?\\.", '', colname)
+remove_kobo_grouper<-function(colname, max_prefix_length){
+  new_name<-sub(".*?\\.", '', colname)
+  name_df<-data.frame(old_name=colname, new_name=new_name)
+  name_df<-name_df %>%
+    mutate(
+      char_diff=stringr::str_length(old_name)-stringr::str_length(new_name),
+      new_col_name=if_else(char_diff<=max_prefix_length, as.character(new_name),as.character(old_name))
+    )
+  name_df$new_col_name %>% return()
 }
+
+
+#old one
+# remove_kobo_grouper<-function(colname){
+#   sub(".*?\\.", '', colname)
+# }
