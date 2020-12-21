e4da74e5f8d99f599e4e2795fc05c5c2a9d31b9c
zackarno <zackarno@gmail.com>
2020-12-21 14:02:21 +0300

    add assertion and hint message

---
+++ R/check_others.R
@@ -0,0 +1,32 @@
+#' Check variation by: check the variation of all columns by grouping var (specifically designed for the grouping var to be the data collectors identifier). Variation results are checked with resepect to standard deviations of them mean and a plot is produced to help understand issues.
+#' @param df data frame
+#' @param suffix suffx that ends other columns (prefere kobos to be built with _other suffix)
+#' @return cleaning log
+#' @export
+
+
+check_others<- function(df, suffix="_other", report_cols){
+
+  rep_cols_not_in_df<-report_cols[!report_cols %in% colnames(df)] %>% paste(collapse = ", ")
+  message(crayon::magenta(glue::glue("{rep_cols_not_in_df} are not in data frame")))
+
+  user_uuid<-grep(x = rep_cols_not_in_df,pattern = "uuid", value=T)
+  if(length(user_uuid>0)){
+    data_uuid<-df %>% select(contains("uuid")) %>% colnames()
+    message(crayon::blue(glue::glue("did you mean {data_uuid} as the uuid column?")))
+  }
+  assertthat::assert_that(length(rep_cols_not_in_df)==0,msg= "reporting cols listed above not in data frame")
+
+
+  rep_cols_not_in_df %>% select(contains("uuid"))
+  uuid_col<- df %>% select(matches("uuid")) %>% colnames()
+
+  df %>%
+    filter_at(vars(ends_with(suffix)), any_vars(!is.na(.))) %>%
+    purrr::discard(~all(is.na(.))) %>%
+    select(uuid_col,ends_with(suffix)) %>%
+    mutate(across(as.character(.data))) %>%
+    pivot_longer(-uuid_col) %>%
+    filter(!is.na(value))
+}
+
