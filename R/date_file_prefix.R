150b28ce418ea28a806a3e94474e515af753dca7
zackarno <zackarno@gmail.com>
2021-02-25 12:43:27 +0300

    make date_file_prefix more flexible

--- R/date_file_prefix.R
+++ R/date_file_prefix.R
@@ -1,13 +1,13 @@
 #' Use date string to prefix file names
 
-#' @param date date object (default = Sys.Date())
+#' @param days_from_present number of days from present. Default is today 0. Tomorrow would be 1, yesterday= -1
 #' @return date formated ISO86031 no space
 #' @export
 
 
-
-date_file_prefix<-function(date=Sys.Date()){
-  isodate_file_prefix<-date %>% stringr::str_replace_all("-","")
+date_file_prefix<-function(days_from_present=0){
+  isodate_file_prefix<-(Sys.Date()+days_from_present) %>%
+    stringr::str_replace_all("-","")
   return(isodate_file_prefix)
 }
 
